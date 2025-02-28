9ec1f452ba468c70d7a63184546e59e3f7eae407
chenxuehong <hong_464817688@qq.com>
2025-02-28 18:28:10 +0800

    最后一次提交

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 if __name__ == "__main__":
 
     args = ["./TestCase/home", "-s", "--alluredir=./Report/allure-results", "--clean-alluredir"]
+
     pytest.main(args)
     os.system(r"allure generate --clean ./Report/allure-results/ -o ./Report/html")
     os.system(r"allure serve ./Report/allure-results")
